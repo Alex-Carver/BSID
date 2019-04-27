75bf0d79a5c764ffc0dddd3a4133da1b3ad5a451
Alex <Alex-Carver@.noreply.github.com>
2019-04-27 12:22:20 -0500

    nav scroll color

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
 
 
 $(".scrollTo").on('click', function(e) {
