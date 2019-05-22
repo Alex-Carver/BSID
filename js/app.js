d68dfc68e4ba1765e7d73ed15911f1a6539446e7
Alex <Alex-Carver@.noreply.github.com>
2019-05-22 16:56:14 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -46,3 +46,74 @@ scrollWaypointInit( $('.animateMe') );
 
 });
 
+//Right1 and Left1 controls
+
+  $(function(){
+        $('#v-pills-home-tab').click(function(){
+            $('#right1').show();
+            $('#left1').show();
+        });
+    });
+
+  $(function(){
+        $('#v-pills-messages-tab').click(function(){
+            $('#right1').hide();
+            $('#left1').hide();
+        });
+    });
+
+     $(function(){
+        $('#v-pills-settings-tab').click(function(){
+            $('#right1').hide();
+            $('#left1').hide();
+        });
+    });
+
+
+//Right2 and Left2 controls
+  $(function(){
+        $('#v-pills-messages-tab').click(function(){
+            $('#right2').show();
+            $('#left2').show();
+        });
+    });
+
+    $(function(){
+        $('#v-pills-home-tab').click(function(){
+            $('#right2').hide();
+            $('#left2').hide();
+        });
+    });
+
+       $(function(){
+        $('#v-pills-settings-tab').click(function(){
+            $('#right2').hide();
+            $('#left2').hide();
+        });
+    });
+
+//Right3 and Left3 controls
+
+  $(function(){
+        $('#v-pills-settings-tab').click(function(){
+            $('#right3').show();
+            $('#left3').show();
+        });
+    });
+
+  $(function(){
+        $('#v-pills-messages-tab').click(function(){
+            $('#right3').hide();
+            $('#left3').hide();
+        });
+    });
+
+    $(function(){
+        $('#v-pills-home-tab').click(function(){
+            $('#right3').hide();
+            $('#left3').hide();
+        });
+    });
+
+ 
+
